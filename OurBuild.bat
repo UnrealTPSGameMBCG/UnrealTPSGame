be5d6f2af66ede7a8a2c0fd147397c6bdc720b39
MichaelBCG <example@example.com>
2023-12-08 02:51:43 +0100

    Initial commit UE5.3 compatible

---
+++ OurBuild.bat
@@ -0,0 +1,7 @@
+"c:\Program Files\Epic Games\UE_5.3\Engine\Build\BatchFiles\RunUAT.bat" ^
+BuildCookRun ^
+-project="c:\Users\michael\Documents\Unreal Projects\2023-12-05_TPSGame_Automation\UnrealTPSGame\UnrealTPSGame\TPS.uproject" ^
+-platform=Win64 ^
+-clientconfig=Shipping ^
+-archivedirectory="c:\Users\michael\Documents\Unreal Projects\2023-12-05_TPSGame_Automation\UnrealTPSGame\UnrealTPSGame\BuildFromBat" ^
+-build -cook -stage -archive -pak -package -allmaps
--- Source/TPS/TPSCharacter.cpp
+++ Source/TPS/TPSCharacter.cpp
@@ -4,7 +4,9 @@
 
 #pragma warning(disable : 4668)
 #if MY_VR_MACRO
-#include "HeadMountedDisplayFunctionLibrary.h"
+// #include "HeadMountedDisplayFunctionLibrary.h"
+// to make compatible with UE5.3
+#include "HeadMountedDisplay.h"
 #endif
 
 #include "Camera/CameraComponent.h"
@@ -95,7 +97,8 @@ void ATPSCharacter::OnResetVR()
 // or:
 //		Comment or delete the call to ResetOrientationAndPosition below (appropriate if not supporting VR)
 #if MY_VR_MACRO
-    UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
+// COP: mike: I could not find how to call ResetOrientationAndPosition() in UE5.3 
+//UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
 #endif
 }
 
