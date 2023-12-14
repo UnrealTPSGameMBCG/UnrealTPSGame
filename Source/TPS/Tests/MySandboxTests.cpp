e76e0a2ed444fedbbae80b34bfa2d245a4fe31ef
MichaelBCG <example@example.com>
2023-12-14 03:04:22 +0100

    L13. Add first unit test

---
+++ Source/TPS/Tests/MySandboxTests.cpp
@@ -0,0 +1,38 @@
+// My game copyright
+
+#include "Tests/MySandboxTests.h"
+#include "Misc/AutomationTest.h"
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(FMyMathSin, "TPSGame.MyMath.Sin",
+    EAutomationTestFlags::EditorContext | EAutomationTestFlags::ProductFilter | EAutomationTestFlags::HighPriority);
+
+bool FMyMathSin::RunTest(const FString& Parameters)
+{
+    AddInfo("FMath::Sin testing");
+
+    // just experimenting
+    TestTrue("Sin(0) equals 0 [1]", FMath::Sin(0.0) == 0.0);
+    TestTrueExpr(FMath::Sin(0.0) == 0.0);  // just to see difference between TestTrueExpr and TestTrue
+    TestEqual("Sin(0) equals 0 [2]", FMath::Sin(0.0), 0.0);
+
+    // output warning example
+    if (FMath::Sin(0.0) != 0.1)
+    {
+        AddWarning("Just a warning example");
+        return true;  // if return "false" then the test will fail, whereas true allows to use comment and warning.
+    }
+
+    return true;  // it should always return true, as test failures should be tracked in functions like TestTrue etc
+}
+
+/*
+* // we don't need default-generated class
+*
+MySandboxTests::MySandboxTests()
+{
+}
+
+MySandboxTests::~MySandboxTests()
+{
+}
+*/
---
+++ Source/TPS/Tests/MySandboxTests.h
@@ -0,0 +1,20 @@
+// My game copyright
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * My class for testing basic unit testing in UE
+ */
+
+/*
+* // we don't need default-generated class
+*
+class TPS_API MySandboxTests
+{
+public:
+    MySandboxTests();
+    ~MySandboxTests();
+};
+*/
