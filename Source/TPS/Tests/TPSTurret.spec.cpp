f02c36c3368bb9644aac91e8cf0141a82ba62aff
MichaelBCG <example@example.com>
2024-01-19 17:17:44 +0100

    Add turret initial original Spec testing

--- Source/TPS/Tests/TPSTurret.spec.cpp
+++ Source/TPS/Tests/TPSTurret.spec.cpp
@@ -150,6 +150,92 @@ void FTurret::Define()
                     TestDone.Execute();
                 });
         });
+    /*
+    // mike: trying to debug why LatentIt does not work in UE5.3.
+    // .. see https://forums.unrealengine.com/t/automated-tests-are-broken-ue-5-3/1371033See
+    Describe("Debug",
+        [this]()
+        {
+            const int32 InitialAmmoCount = 1;
+            const float FireFreq = 1.5f;
+            LatentBeforeEach(
+                [this, InitialAmmoCount, FireFreq](const FDoneDelegate& TestDone)
+                {
+                    AutomationOpenMap(MapName);
+                    World = GetTestGameWorld();
+                    TestNotNull("World exists", World);
+
+                    Turret = CreateBlueprint<ATPSTurret>(World, TurretBPTestName, InitialTransform);
+                    TestNotNull("Turret exists", Turret);
+                    CallFuncByNameWithParams(
+                        Turret, "SetTurretData", {FString::FromInt(InitialAmmoCount), FString::SanitizeFloat(FireFreq)});
+                    TestDone.Execute();
+
+                    UE_LOG(LogTemp, Display, TEXT("Turret: LatentBeforeEach - OK"));
+                });
+
+            LatentIt(FString::Printf(TEXT("Should be empty after %i sec"), FMath::RoundToInt(InitialAmmoCount * FireFreq)),
+                EAsyncExecution::ThreadPool,
+                [this, InitialAmmoCount, FireFreq](const FDoneDelegate& TestDone)
+                {
+                    AsyncTask(ENamedThreads::GameThread,
+                        [&]()
+                        {
+                            const int32 AmmoCount = GetPropertyValueByName<ATPSTurret, int32>(Turret, "AmmoCount");
+                            TestTrueExpr(AmmoCount == InitialAmmoCount);
+
+                            const FTimerHandle FireTimerHandle =
+                                GetPropertyValueByName<ATPSTurret, FTimerHandle>(Turret, "FireTimerHandle");
+                            TestTrueExpr(World->GetTimerManager().IsTimerActive(FireTimerHandle));
+
+                            UE_LOG(LogTemp, Display, TEXT("Turret: AmmoCount == InitialAmmoCount checked"));
+                        });
+
+                    UE_LOG(LogTemp, Display, TEXT("Turret: Sleep started"));
+                    const float SynchDelta = 0.5f;
+                    FPlatformProcess::Sleep(InitialAmmoCount * FireFreq + SynchDelta);
+                    UE_LOG(LogTemp, Display, TEXT("Turret: Sleep finished"));
+
+                    AsyncTask(ENamedThreads::GameThread,
+                        [&]()
+                        {
+                            const int32 AmmoCount = GetPropertyValueByName<ATPSTurret, int32>(Turret, "AmmoCount");
+                            TestTrueExpr(AmmoCount == 0);
+                            const FTimerHandle FireTimerHandle =
+                                GetPropertyValueByName<ATPSTurret, FTimerHandle>(Turret, "FireTimerHandle");
+                            TestTrueExpr(!World->GetTimerManager().IsTimerActive(FireTimerHandle));
+
+                            UE_LOG(LogTemp, Display, TEXT("Turret: AmmoCount == 0 checked"));
+                        });
+
+                    TestDone.Execute();
+                });
+
+            LatentIt(FString::Printf(TEXT("Should simply run")),
+                [this, InitialAmmoCount, FireFreq](const FDoneDelegate& TestDone)
+                {
+                    UE_LOG(LogTemp, Display, TEXT("Turret: Should simply run 1 - OK"));
+
+                    UE_LOG(LogTemp, Display, TEXT("Turret: Sleep started"));
+                    const float SynchDelta = 0.5f;
+                    FPlatformProcess::Sleep(6.5f);  // sleep in sec
+                    UE_LOG(LogTemp, Display, TEXT("Turret: Sleep finished"));
+
+                    AsyncTask(ENamedThreads::GameThread, [&]() { UE_LOG(LogTemp, Display, TEXT("Turret: Should simply run 2 - OK")); });
+
+                    TestDone.Execute();
+                });
+
+            LatentAfterEach(
+                [this](const FDoneDelegate& TestDone)
+                {
+                    SpecCloseLevel(World);
+                    TestDone.Execute();
+
+                    UE_LOG(LogTemp, Display, TEXT("Turret: LatentAfterEach - OK"));
+                });
+        });
+    */ // END: mike: trying to debug why LatentIt does not work in UE5.3
 }
 
 #endif
