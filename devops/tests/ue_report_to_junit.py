b0ab303b5a5e0d429c2e257393a663ae4bf0b8ec
MichaelBCG <example@example.com>
2024-02-07 01:49:27 +0100

    Modify ue_report_to_junit.py to handle no json file generated (no tests run)

--- devops/tests/ue_report_to_junit.py
+++ devops/tests/ue_report_to_junit.py
@@ -2,6 +2,8 @@ import argparse
 import json
 import xml.etree.ElementTree as xmlTree
 import xml.dom.minidom as miniDom
+# os is used for checking if json file exist
+import os 
 
 # unreal has test report in specific json format
 # we need to convert that json to junit format to publish it in Jenkins
@@ -20,65 +22,84 @@ def main():
     args = parser.parse_args()
 
     # load JSON
-    json_file = open(args.ue_report_path, encoding="utf-8-sig")
-    json_obj = json.loads(json_file.read())
-    # print(json_obj)
+    #json_file = open(args.ue_report_path, encoding="utf-8-sig")
+    #json_obj = json.loads(json_file.read())
+    #print(json_obj)
 
-    # create xml root
-    xml_root = xmlTree.Element(
-        "testsuites",
-        name="Project Tests",
-        tests=str(len(json_obj["tests"])),
-        failures=str(json_obj["failed"]),
-        time=str(json_obj["totalDuration"]),
-        timestamp=json_obj["reportCreatedOn"],
-    )
+    # if json file does not exist, then simply скуфеу an empty xml file
+    json_file_open_successful = True
 
-    # combine all UE tests
-    tests = {}
-    errors = {}
-    for test in json_obj["tests"]:
-        test_name = test["testDisplayName"]
-        test_suite_name = test["fullTestPath"][: test["fullTestPath"].find(test_name) - 1]
+    if os.path.exists(args.ue_report_path):
+        json_file = open(args.ue_report_path, encoding="utf-8-sig")
+        json_obj = json.loads(json_file.read())
+        print(json_obj)
+    else:
+        print("Error: The specified JSON file does not exist.")
+        json_file_open_successful = False
 
-        test_suite = tests[test_suite_name] if test_suite_name in tests else {}
-
-        if test["errors"] == 0:
-            test_suite.update({test_name: {}})
-        else:
-            error_msg = create_error_msg(test)
-            error_count = 1 if test_suite_name not in errors else errors[test_suite_name] + 1
-            errors.update({test_suite_name: error_count})
-            test_suite.update({test_name: error_msg})
-
-        tests.update({test_suite_name: test_suite})
-    # print(tests)
-
-    # convert to xml nodes
-    for test_suite in tests:
-        errs_number = errors[test_suite] if test_suite in errors else 0
-        tests_count_in_suite=len(tests[test_suite])
-
-        xml_test_suite = xmlTree.SubElement(
-            xml_root,
-            "testsuite",
-            name=test_suite,
-            tests=str(tests_count_in_suite),
-            failures=str(errs_number),
-            disabled="0",
+    if json_file_open_successful:
+        # create xml root
+        xml_root = xmlTree.Element(
+            "testsuites",
+            name="Project Tests",
+            tests=str(len(json_obj["tests"])),
+            failures=str(json_obj["failed"]),
+            time=str(json_obj["totalDuration"]),
+            timestamp=json_obj["reportCreatedOn"],
         )
 
-        for test_case in tests[test_suite]:
-            xml_test_case = xmlTree.SubElement(xml_test_suite, "testcase", name=test_case, classname=test_suite)
-            if tests[test_suite][test_case]:
-                xmlTree.SubElement(xml_test_case, "failure", message=tests[test_suite][test_case])
+        # combine all UE tests
+        tests = {}
+        errors = {}
+        for test in json_obj["tests"]:
+            test_name = test["testDisplayName"]
+            test_suite_name = test["fullTestPath"][: test["fullTestPath"].find(test_name) - 1]
 
-    # write output XML
-    xml_output = miniDom.parseString(xmlTree.tostring(xml_root, encoding="utf8", method="xml"))
-    # print(xml_output.toprettyxml(indent="  "))
+            test_suite = tests[test_suite_name] if test_suite_name in tests else {}
 
-    xml_file = open(args.junit_xml_path, "w")
-    xml_file.write(xml_output.toprettyxml(indent="  "))
+            if test["errors"] == 0:
+                test_suite.update({test_name: {}})
+            else:
+                error_msg = create_error_msg(test)
+                error_count = 1 if test_suite_name not in errors else errors[test_suite_name] + 1
+                errors.update({test_suite_name: error_count})
+                test_suite.update({test_name: error_msg})
+
+            tests.update({test_suite_name: test_suite})
+        print(tests)
+
+        # convert to xml nodes
+        for test_suite in tests:
+            errs_number = errors[test_suite] if test_suite in errors else 0
+            tests_count_in_suite=len(tests[test_suite])
+
+            xml_test_suite = xmlTree.SubElement(
+                xml_root,
+                "testsuite",
+                name=test_suite,
+                tests=str(tests_count_in_suite),
+                failures=str(errs_number),
+                disabled="0",
+            )
+
+            for test_case in tests[test_suite]:
+                xml_test_case = xmlTree.SubElement(xml_test_suite, "testcase", name=test_case, classname=test_suite)
+                if tests[test_suite][test_case]:
+                    xmlTree.SubElement(xml_test_case, "failure", message=tests[test_suite][test_case])
+
+        # write output XML
+        xml_output = miniDom.parseString(xmlTree.tostring(xml_root, encoding="utf8", method="xml"))
+        print(xml_output.toprettyxml(indent="  "))
+
+        xml_file = open(args.junit_xml_path, "w")
+        xml_file.write(xml_output.toprettyxml(indent="  "))
+
+    # if json_file_open_successful is not true, just write an empty xml file
+    else: 
+        os.makedirs(os.path.dirname(args.junit_xml_path), exist_ok=True)
+        xml_file = open(args.junit_xml_path, "w")
+        xml_file.close()
+
 
 def create_error_msg(test):
     error_msg = ""
