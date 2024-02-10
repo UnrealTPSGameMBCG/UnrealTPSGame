f500975945547da86ef5b01786a62e15f6d72283
MichaelBCG <example@example.com>
2024-02-11 00:42:56 +0100

    Add TMP .bat for test coverage

--- devops/tests/run_tests_coverageTMP.bat
+++ devops/tests/run_tests_coverageTMP.bat
@@ -1,19 +1,8 @@
+rem Just run tests and run test coverage, assuming that UE project is already built 
+
 @echo off
 
 call "%~dp0..\config.bat"
-
-:: https://stackoverflow.com/a/20999154
-set Before=public bool UnoptimizedCode = false;
-set After=public bool UnoptimizedCode = true;
-set File=%SourceCodePath%\%ProjectPureName%Editor.Target.cs
-powershell -Command "(gc '%File%') -replace '%Before%', '%After%' | Out-File  %File%"
-
-rem build sources
-call "%RunUATPath%" BuildCookRun ^
--project="%ProjectPath%" ^
--platform="%Platform%" ^
--clientconfig="%Configuration%" ^
--build -cook -ubtargs="-UnoptimizedCode" -noturnkeyvariables
 
 rem run tests
 set TestRunner="%EditorPath%" "%ProjectPath%" -ExecCmds="Automation RunTests %TestNames%;Quit" ^
@@ -33,21 +22,21 @@ call :NORMALIZEPATH %ExludedPathForTestReport%
 set ExludedSources=%RETVAL%
 
 "%OpenCPPCoveragePath%" --modules="%Module%" --sources="%Sources%" ^
---excluded_sources="%ExludedSources%" --export_type="%ExportType%" -v -- %TestRunner%
+--excluded_sources="%ExludedSources%" --export_type="%ExportType%" -- %TestRunner% -v 
 
-rem clean obsolete artifacts
+rem clean obsolete artifacts of test coverage
 del /q LastCoverageResults.log
-powershell -Command "(gc %File%) -replace '%After%', '%Before%' | Out-File  %File%"
 
 rem copy test artifacts
 set TestsDir=%~dp0
 set TestsDataDir=%~dp0data
 robocopy "%TestsDataDir%" "%ReportOutputPath%" /E
 
-rem start local server and show report
+rem start local server and show test report
 set Port=8081
 set Localhost=http://localhost:%Port%
 
+rem running HTML reports with tests results and test coverage
 pushd "%ReportOutputPath%"
 start "" "%Localhost%"
 start "" "%Localhost%\Coverage\CodeCoverageReport\index.html"
@@ -57,7 +46,7 @@ popd
 :: ========== FUNCTIONS ==========
 goto:EOF
 
-rem returning the full paths 
+:: returning the full paths 
 :NORMALIZEPATH
   SET RETVAL=%~f1
   EXIT /B
